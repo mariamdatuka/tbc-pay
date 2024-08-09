717fbd7a5269f1e00113e9218d68361bd4a26bb2
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-08-09 21:39:23 +0400

    chore

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 const Card = () => {
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate("/form");
+  };
   return (
     <>
-      <section className="flex items-center justify-center h-36 p-7 rounded-lg cursor-pointer font-semibold shadow-md bg-white hover:shadow-xl transition-shadow easy-in duration-300">
-        <h1 className="text-darkBlue font-semibold text-2xl">
+      <section
+        className="flex items-center justify-center h-36 p-3 sm:p-7 rounded-lg cursor-pointer font-semibold shadow-md bg-white hover:shadow-xl transition-shadow easy-in duration-300"
+        onClick={handleNavigation}
+      >
+        <h1 className="text-darkBlue font-semibold text-lg sm:text-xl">
           დაიწყე ფორმის შევსება
         </h1>
       </section>
